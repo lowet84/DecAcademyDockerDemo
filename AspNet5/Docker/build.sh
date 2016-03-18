8bd7a4153213042570e689338676b71842641884
root <lowet84@gmail.com>
2016-03-18 13:16:11 +0100

    fslijdf

---
+++ AspNet5/Docker/build.sh
@@ -0,0 +1,4 @@
+mkdir -p Content
+
+docker build -t lowet84/decacademyaspnet5 .
+rm -r Content
---
+++ AspNet5/Docker/run.sh
@@ -0,0 +1,2 @@
+docker rm -f decacademy_aspnet5
+docker run --name decacademy_aspnet5 -it -d -p 5000:5000 lowet84/decacademyaspnet5
